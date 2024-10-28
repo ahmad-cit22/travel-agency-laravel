818c1f3fa8cd459f442811be27c95f323c0427c3
Nafis Abdullah Ahmad <n.ahmad.web.cit22@gmail.com>
2024-10-28 15:02:15 +0600

    homepage templating

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -1,22 +1,24 @@
 <!-- Jquery -->
-    <script src="assets/js/jquery-3.6.0.min.js"></script>
-    <!-- Bootstrap -->
-    <script src="assets/js/bootstrap.min.js"></script>
-    <!-- Appear Js -->
-    <script src="assets/js/appear.min.js"></script>
-    <!-- Slick -->
-    <script src="assets/js/slick.min.js"></script>
-    <!-- Magnific Popup -->
-    <script src="assets/js/jquery.magnific-popup.min.js"></script>
-    <!-- Nice Select -->
-    <script src="assets/js/jquery.nice-select.min.js"></script>
-    <!-- Image Loader -->
-    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
-    <!-- Skillbar -->
-    <script src="assets/js/skill.bars.jquery.min.js"></script>
-    <!-- Isotope -->
-    <script src="assets/js/isotope.pkgd.min.js"></script>
-    <!--  AOS Animation -->
-    <script src="assets/js/aos.js"></script>
-    <!-- Custom script -->
-    <script src="assets/js/script.js"></script>
+<script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}"></script>
+<!-- Bootstrap -->
+<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+<!-- Appear Js -->
+<script src="{{ asset('assets/js/appear.min.js') }}"></script>
+<!-- Slick -->
+<script src="{{ asset('assets/js/slick.min.js') }}"></script>
+<!-- Magnific Popup -->
+<script src="{{ asset('assets/js/jquery.magnific-popup.min.js') }}"></script>
+<!-- Nice Select -->
+<script src="{{ asset('assets/js/jquery.nice-select.min.js') }}"></script>
+<!-- Image Loader -->
+<script src="{{ asset('assets/js/imagesloaded.pkgd.min.js') }}"></script>
+<!-- Skillbar -->
+<script src="{{ asset('assets/js/skill.bars.jquery.min.js') }}"></script>
+<!-- Isotope -->
+<script src="{{ asset('assets/js/isotope.pkgd.min.js') }}"></script>
+<!--  AOS Animation -->
+<script src="{{ asset('assets/js/aos.js') }}"></script>
+<!-- Custom script -->
+<script src="{{ asset('assets/js/script.js') }}"></script>
+
+@stack('custom-scripts')
