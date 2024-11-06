a24441d45caf9ba19e193cdaab70c42472b9ce5f
Nafis Abdullah Ahmad <n.ahmad.web.cit22@gmail.com>
2024-11-06 17:38:10 +0600

    update contact contents

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -24,25 +24,6 @@
     </section>
     <!-- Page Banner End -->
 
-    <!-- Page Banner Start -->
-    <section class="page-banner-area pt-50 pb-35 rel z-1 bgs-cover"
-        style="background-image: url(assets/images/banner/banner.jpg);">
-        <div class="container">
-            <div class="banner-inner text-white">
-                <h2 class="page-title mb-10" data-aos="fade-left" data-aos-duration="1500" data-aos-offset="50">Contact Us
-                </h2>
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb justify-content-center mb-20" data-aos="fade-right" data-aos-delay="200"
-                        data-aos-duration="1500" data-aos-offset="50">
-                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                        <li class="breadcrumb-item active">Contact Us</li>
-                    </ol>
-                </nav>
-            </div>
-        </div>
-    </section>
-    <!-- Page Banner End -->
-
 
     <!-- Contact Info Area start -->
     <section class="contact-info-area pt-100 rel z-1">
@@ -52,20 +33,19 @@
                     <div class="contact-info-content mb-30 rmb-55" data-aos="fade-up" data-aos-duration="1500"
                         data-aos-offset="50">
                         <div class="section-title mb-30">
-                            <h2>Let’s Talk Our Expert Travel Guides</h2>
+                            <h2>Reach Out to Our Hajj Experts</h2>
                         </div>
-                        <p>Our dedicated support team is always ready to assist you with any questions or issues, offering
-                            prompt and personalized solutions to meet your needs.</p>
+                        <p>Our devoted team is here to guide you through every step of your Hajj journey, ensuring a smooth and fulfilling experience.</p>
                         <div class="features-team-box mt-40">
-                            <h6>85+ Expert Team member</h6>
+                            <h6>85+ Dedicated Hajj Guides</h6>
                             <div class="feature-authors">
-                                <img src="assets/images/features/feature-author1.jpg" alt="Author">
-                                <img src="assets/images/features/feature-author2.jpg" alt="Author">
-                                <img src="assets/images/features/feature-author3.jpg" alt="Author">
-                                <img src="assets/images/features/feature-author4.jpg" alt="Author">
-                                <img src="assets/images/features/feature-author5.jpg" alt="Author">
-                                <img src="assets/images/features/feature-author6.jpg" alt="Author">
-                                <img src="assets/images/features/feature-author7.jpg" alt="Author">
+                                <img src="assets/images/features/feature-author1.jpg" alt="Guide">
+                                <img src="assets/images/features/feature-author2.jpg" alt="Guide">
+                                <img src="assets/images/features/feature-author3.jpg" alt="Guide">
+                                <img src="assets/images/features/feature-author4.jpg" alt="Guide">
+                                <img src="assets/images/features/feature-author5.jpg" alt="Guide">
+                                <img src="assets/images/features/feature-author6.jpg" alt="Guide">
+                                <img src="assets/images/features/feature-author7.jpg" alt="Guide">
                                 <span>+</span>
                             </div>
                         </div>
@@ -78,9 +58,9 @@
                                 data-aos-delay="50">
                                 <div class="icon"><i class="fas fa-envelope"></i></div>
                                 <div class="content">
-                                    <h5>Need Help & Support</h5>
+                                    <h5>Inquire About Hajj Services</h5>
                                     <div class="text"><i class="far fa-envelope"></i> <a
-                                            href="mailto:support@gmail.com">support@gmail.com</a></div>
+                                            href="mailto:support@hajjservices.com">support@hajjservices.com</a></div>
                                 </div>
                             </div>
                         </div>
@@ -89,7 +69,7 @@
                                 data-aos-delay="100">
                                 <div class="icon"><i class="fas fa-phone"></i></div>
                                 <div class="content">
-                                    <h5>Need Any Urgent</h5>
+                                    <h5>Immediate Assistance</h5>
                                     <div class="text"><i class="far fa-phone"></i> <a href="callto:+0001234588">+000 (123)
                                             45 88</a></div>
                                 </div>
@@ -100,7 +80,7 @@
                                 data-aos-delay="50">
                                 <div class="icon"><i class="fas fa-map-marker-alt"></i></div>
                                 <div class="content">
-                                    <h5>New York Branch</h5>
+                                    <h5>New York Hajj Office</h5>
                                     <div class="text"><i class="fal fa-map-marker-alt"></i> 55 East 10th Street, New York,
                                         NY 10003, United States</div>
                                 </div>
@@ -132,10 +112,10 @@
                 <div class="col-lg-7">
                     <div class="comment-form bgc-lighter z-1 rel mb-30 rmb-55">
                         <form id="contactForm" class="contactForm" name="contactForm"
-                            action="https://webtendtheme.net/html/2024/ravelo/assets/php/form-process.php" method="post"
+                            action="" method="post"
                             data-aos="fade-left" data-aos-duration="1500" data-aos-offset="50">
                             <div class="section-title">
-                                <h2>Get In Touch</h2>
+                                <h2>Connect with Us</h2>
                             </div>
                             <p>Your email address will not be published. Required fields are marked *</p>
                             <div class="row mt-35">
@@ -200,18 +180,14 @@
                         data-aos-offset="50">
                         <div class="row">
                             <div class="col-12">
-                                <img src="assets/images/contact/contact1.jpg" alt="Contact">
+                                <img src="assets/images/contact/contact1.jpg" alt="Contact" style="border-radius: 20px !important">
                             </div>
                             <div class="col-6">
-                                <img src="assets/images/contact/contact2.jpg" alt="Contact">
+                                <img src="assets/images/contact/contact2.jpg" alt="Contact" style="border-radius: 20px !important" height="300">
                             </div>
                             <div class="col-6">
-                                <img src="assets/images/contact/contact3.jpg" alt="Contact">
+                                <img src="assets/images/contact/contact3.jpg" alt="Contact" style="border-radius: 20px !important" height="300">
                             </div>
-                        </div>
-                        <div class="circle-logo">
-                            <img src="assets/images/contact/icon.png" alt="Logo">
-                            <span class="title h2">Ravelo</span>
                         </div>
                     </div>
                 </div>
