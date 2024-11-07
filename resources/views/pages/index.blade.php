f632c5692c834e5b9e0abf9157bf805f81b8445c
Nafis Abdullah Ahmad <n.ahmad.web.cit22@gmail.com>
2024-11-07 11:55:25 +0600

    update

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -6,7 +6,7 @@
         style="background-color: #181622 !important; background-repeat: no-repeat; background-image: url({{ asset('assets/images/hero/pxfuel1.jpg') }}); background-size: cover;">
         <!-- Gradient Overlay -->
         <div
-            style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background: linear-gradient(to bottom, #241f3e, #0e0e18); opacity: 0.7; z-index: 1;">
+            style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background: linear-gradient(to bottom, var(--primary-color), var(--heading-color)); opacity: 0.7; z-index: 1;">
         </div>
         <div class="container-fluid">
             <h1 class="hero-title" data-aos="flip-up" data-aos-delay="50" data-aos-duration="1500" data-aos-offset="50">Hajj &
