18d2d98c3bed7c3d587a82fe8be8cea773c656e9
Nafis Abdullah Ahmad <n.ahmad.web.cit22@gmail.com>
2024-11-24 14:59:01 +0600

    update package mirgation

--- database/migrations/2024_11_23_123244_create_packages_table.php
+++ database/migrations/2024_11_23_123244_create_packages_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('packages', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('guide_id')->constrained()->onDelete('cascade');
+            $table->date('departure_date');
+            $table->date('return_date');
+            $table->string('departure_location');
+            $table->string('return_location');
+            $table->decimal('price_adult', 8, 2);
+            $table->decimal('price_child', 8, 2);
+            $table->integer('total_seats');
+            $table->integer('booked_seats')->default(0);
+            $table->string('status')->default('pending')->comment('pending, active, inactive');
             $table->timestamps();
         });
     }
